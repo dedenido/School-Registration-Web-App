4d99447c5bd7216ab1e716f0e3c35e2ca748f844
Peter Panic <peter.panic@email.com>
2014-09-23 18:14:15 +0800

    Additional servlet for Section Creation

---
+++ src/main/java/com/orangeandbronze/schoolreg/servlets/CreateSectionServlet.java
@@ -0,0 +1,33 @@
+package com.orangeandbronze.schoolreg.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.orangeandbronze.schoolreg.service.CreateSectionService;
+import com.orangeandbronze.schoolreg.service.EnlistService;
+
+/**
+ * Servlet implementation class CreateSectionServlet
+ */
+@WebServlet("/createSection")
+public class CreateSectionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+//		getServletContext().getRequestDispatcher("/WEB-INF/sectioncreation.jsp").forward(request, response);
+		getServletContext().getRequestDispatcher("/WEB-INF/sectioncreation.jsp").forward(request, response);
+
+	}
+		
+
+}
---
+++ src/main/java/com/orangeandbronze/schoolreg/servlets/NewSection.java
@@ -0,0 +1,54 @@
+package com.orangeandbronze.schoolreg.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class NewSection
+ */
+@WebServlet("/newSection")
+public class NewSection extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	
+//		HttpSession session = request.getSession();
+//		
+//		String sectionName = (String)session.getAttribute("section");
+//		String teacher = (String)session.getAttribute("teacher");
+//		String subject = (String)session.getAttribute("subject");
+//		String subjectprerequisite = (String)session.getAttribute("subprerequisite");
+//		String schedule = (String)session.getAttribute("schedule");
+		
+		String sectionName = request.getParameter("sectionname");
+		String teacher = request.getParameter("teacher");
+		String subject = request.getParameter("subject");
+		String subjectPrerequisite = request.getParameter("subjectprerequisite");
+		String schedule = request.getParameter("schedule");
+		
+		
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		writer.println(sectionName + "successfully added with the following details" );
+		writer.println("Teacher: " + teacher);
+		writer.println("Subject: " + subject);
+		writer.println("subjectprerequisite: " + subjectPrerequisite);
+		writer.println("Schedule" + schedule);
+		
+		writer.flush();
+	
+	
+	}
+
+}
