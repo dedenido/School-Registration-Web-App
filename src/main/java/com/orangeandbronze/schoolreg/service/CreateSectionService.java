dc62726fa175c31b9bc7e4bb46c6149dd7f63818
dedenido <whyispresident@gmail.com>
2014-09-24 12:58:48 +0800

    Modified CreateSectionService to view faculty

--- src/main/java/com/orangeandbronze/schoolreg/service/CreateSectionService.java
+++ src/main/java/com/orangeandbronze/schoolreg/service/CreateSectionService.java
@@ -1,5 +1,16 @@
 package com.orangeandbronze.schoolreg.service;
 
+import java.util.List;
+
+import com.orangeandbronze.schoolreg.dao.FacultyDao;
+import com.orangeandbronze.schoolreg.domain.Faculty;
+
 public class CreateSectionService {
+	
+	private FacultyDao facultyDao = new FacultyDao();
+	
+	public List<Faculty> getAllFaculty() {
+		return facultyDao.getAll();
+	}
 
 }
